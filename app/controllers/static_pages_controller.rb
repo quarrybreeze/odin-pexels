7978fdfb956a59bed092e4760e1ed46167f53404
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-04-04 14:24:52 -0700

    New: added index controller and view that uses the ruby pexels gem. Users can now search for a collection Id and it will return photos and credit them to their owner.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,3 +1,14 @@
 class StaticPagesController < ApplicationController
-  
+  def index
+    if collection_params[:collection_id].present?
+      client = Pexels::Client.new(ENV['PEXELS_API_KEY'])
+      @photos = client.collections[collection_params[:collection_id], type: 'photos'].media
+    end
+
+  end
+
+  private
+  def collection_params
+    params.permit(:collection_id,:commit)
+  end
 end
